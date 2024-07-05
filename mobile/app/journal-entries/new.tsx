b19cbb8d256892ac88cb7377eea7ee6b27452c16
brianMunyao <brianmunyao6@gmail.com>
2024-07-05 12:48:40 +0300

    fix(mobile): update store on add

--- mobile/app/journal-entries/new.tsx
+++ mobile/app/journal-entries/new.tsx
@@ -4,14 +4,19 @@ import { router } from 'expo-router';
 import JournalEntryForm from '@/components/JournalEntryForm';
 import { IJournalEntryNew } from '@/types/IJournalEntry';
 import journalEntriesService from '@/api/services/journalEntriesService';
+import { useAppDispatch } from '@/store/hooks';
+import { createJournalEntry } from '@/store/slices/journalEntriesSlice';
 
 export default function NewJournalEntryPage() {
 	const [formError, setFormError] = useState('');
 
+	const dispatch = useAppDispatch();
+
 	const handleSubmit = async (values: IJournalEntryNew) => {
 		const response = await journalEntriesService.createJournalEntry(values);
 
-		if (response.success) {
+		if (response.success && response?.data) {
+			dispatch(createJournalEntry(response.data?.journalEntry));
 			router.replace('/');
 		} else {
 			setFormError(response?.message || 'Error occured. Try Again.');
